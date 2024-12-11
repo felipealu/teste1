71f6f107e9921d43306a6737f6a1566525cabc98
felipealui <145783789+felipealu@users.noreply.github.com>
2024-12-11 12:03:32 -0300

    Update leitor.js

--- leitor.js
+++ leitor.js
@@ -3,22 +3,6 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const outputData = document.getElementById("outputData");
 const context = canvas.getContext("2d");
-// Campos do formulário de cadastro
-const searchField = document.getElementById("nome");
-const identificacaoField = document.getElementById("identificacao");
-const veiculoField = document.getElementById("veiculo");
-const placaField = document.getElementById("placa");
-const celularField = document.getElementById("celular");
-const sitEscolaField = document.getElementById("sit_escola");
-const sitServiceField = document.getElementById("sit_service");
-// Arrays para armazenar as informações lidas
-const nomes = [];
-const identificacoes = [];
-const veiculos = [];
-const placas = [];
-const sitescola = [];
-const siteservice = [];
-
 
 // Função para inicializar a câmera traseira
 async function startRearCamera() {
@@ -99,37 +83,6 @@ function tick() {
     } else {
       outputData.innerText = "Aponte para um QR Code.";
     }
-    // Verifica se as informações já foram armazenadas
-    if (
-      !nomes.includes(qrData.name) &&
-      !identificacoes.includes(qrData.identification) &&
-      !veiculos.includes(qrData.vehicle) &&
-      !placas.includes(qrData.placa) &&
-      !sitescola.includes(qrData.sit_escola) &&
-      !siteservice.includes(qrData.sit_service)
-    ) {
-    // Armazena os novos dados nos arrays
-      nomes.push(qrData.name);
-      identificacoes.push(qrData.identification);
-      veiculos.push(qrData.vehicle);
-      placas.push(qrData.placa);
-      sitescola.push(qrData.sit_escola);
-      siteservice.push(qrData.sit_service);
-
-    // Preenche os campos do formulário com os dados do QR code
-      searchField.value = qrData.name || "";
-      identificacaoField.value = qrData.identification || "";
-      veiculoField.value = qrData.vehicle || "";
-      placaField.value = qrData.placa || "";
-      sitEscolaField.value = qrData.sit_escola || "";
-      sitServiceField.value = qrData.sit_service || "";
-
-    // Atualiza a interface para mostrar os dados lidos
-      outputData.innerText = "Dados preenchidos automaticamente.";
-    } else {
-      outputData.innerText = "Código já lido.";
-    }
-
   }
   requestAnimationFrame(tick);
 }
