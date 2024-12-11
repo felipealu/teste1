31fff75dd810bb1e2c40edb89d5e19688f2b12e5
felipealui <145783789+felipealu@users.noreply.github.com>
2024-12-11 13:14:59 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -148,6 +148,8 @@
                 <option value="IGREJA">IGREJA</option>
                 <option value="GREMIO">GREMIO</option>
                 <option value="PNRS">PNRS</option>
+                <option value="ESCOLA">ESCOLA</option>
+
 
             </select><br /><br />
 
